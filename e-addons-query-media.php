6ab495c9092d53432217eb821dc6ba1dbfdad711
fra <fra@fra.dev>
2021-03-26 14:58:21 +0100

    v1.0.4

--- e-addons-query-media.php
+++ e-addons-query-media.php
@@ -6,7 +6,7 @@
  * Plugin Name:       e-addons QUERY MEDIA
  * Plugin URI:        https://e-addons.com
  * Description:       Create your own Media Gallery in a creative and simple way
- * Version:           1.0.3
+ * Version:           1.0.4
  * Author:            Nerds Farm
  * Author URI:        https://nerds.farm
  * Text Domain:       e-addons-query-media
